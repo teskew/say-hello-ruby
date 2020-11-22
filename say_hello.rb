115e6e38418caffc93d2179d609c0dc663ab6b0b
teskew <teskinder1@gmail.com>
2020-11-22 13:35:59 -0600

    commit tree 8e4d141c6016cbbcee2f3982c191db4b6e76adba

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,8 @@
 # Build your say_hello method here
-def say_hello(name ="Hello Ruby Programmer!")
-    puts "Hello, #{name}"
+def say_hello (name = "Ruby Programmer" )
+  
+    puts "Hello #{name}!"
   end 
-  say_hello("Gabriela")
-  say_hello(name)
+  say_hello
+  say_hello("Gabriela ")
+  
